fe5e5410f4bdd820f4f7a20249af7cd6ebd8730a
gds83192 <graham.squires3@gmail.com>
2020-10-27 05:51:18 -0400

    Research Dayjs and render timestamps for the Tweets. -- Completed

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Reference for using app.locals and timestamps https://stackoverflow.com/questions/12794860/how-to-use-node-modules-like-momentjs-in-ejs-views 
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -8,7 +9,8 @@ var moment = require('moment');
 var shortDateFormat = "ddd @ h:mmA"; // this is just an example of storing a date format once so you can change it in one place and have it propagate
 app.locals.moment = moment; // this makes moment available as a variable in every EJS page
 app.locals.shortDateFormat = shortDateFormat;
-
+const dayjs = require('dayjs')
+app.locals.dayjs = require('dayjs');
 const mongoURI = 'mongodb://localhost:27017/mongoRelationships';
 
 mongoose.connect(
